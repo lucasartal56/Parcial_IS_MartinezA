4e3349bd68a17fbcb54758b996a0cc2c0768e308
lucasartal56 <lucdanart@gmail.com>
2024-05-20 10:00:58 -0600

    creacion de footer y haeder

---
+++ CRUD/vistas/templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <title>Registro</title>
+</head>
+<body>
+    <div class="container">
